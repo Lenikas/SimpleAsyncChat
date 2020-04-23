4795fc6b58316b0d6e37e42936ca0552cf4509e3
Lenikas <stuffer232@gmail.com>
2020-04-23 10:55:51 +0500

    fix issue

--- async_chat/app.py
+++ async_chat/app.py
@@ -3,11 +3,12 @@ from typing import Any, List
 
 import redis
 from async_chat.models import Message
-from fastapi import FastAPI, Request, WebSocket, websockets
+from fastapi import FastAPI, HTTPException, Request, WebSocket, websockets
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
-rd = redis.Redis.from_url('redis://0.0.0.0')
+
+rd = redis.Redis()
 rd.lpush('messages', '')
 rd.flushall()
 
@@ -23,10 +24,21 @@ async def get(request: Request, username: str) -> Any:
     )
 
 
-@app.get('/messages/last50')
-async def get_last_messages(request: Request) -> Any:
-    rd.ltrim('messages', 0, 49)
-    messages = rd.lrange('messages', 0, 49)
+@app.get('/messages/last/{count}')
+async def get_last_messages(request: Request, count: str) -> Any:
+    try:
+        count_messages = int(count)
+    except ValueError:
+        raise HTTPException(
+            status_code=404, detail={'Error': 'Count of last messages must be a number'}
+        )
+    if count_messages > 50 or count_messages < 0:
+        raise HTTPException(
+            status_code=404,
+            detail={'Error': 'Count of last messages must be less that 50 and more 0'},
+        )
+
+    messages = rd.lrange('messages', 0, count_messages - 1)
     return templates.TemplateResponse(
         'last_messages_page.html', {'request': request, 'messages': messages}
     )
@@ -36,12 +48,12 @@ async def chat_logic(websocket: WebSocket, username: str) -> None:
     try:
         while True:
             data: str = await websocket.receive_text()
-            model_data = {'user': username, 'text': data, 'date_time': datetime.now()}
-            message = Message(**model_data)
+            message = Message(user=username, text=data, date_time=datetime.utcnow())
             rd.lpush(
                 'messages',
-                f'{message.user} write message: {message.text} on time: {datetime.now()}',
+                f'{message.user} write message: {message.text} on time: {message.date_time}',
             )
+            rd.ltrim('messages', 0, 49)
             for connection in connections:
                 await connection.send_text(f'{username}: {data}')
     except websockets.WebSocketDisconnect:
--- tests/test_app.py
+++ tests/test_app.py
@@ -14,15 +14,20 @@ def test_get(test_client: TestClient):
     assert 'Chat' in response.text
 
 
-def test_get_last_messages_empty(test_client: TestClient):
-    response = test_client.get('/messages/last50')
-    assert response.status_code == 200
+@pytest.mark.parametrize('count_messages', ('100', '-10', 'abc'))
+def test_get_last_messages_bad_request(test_client: TestClient, count_messages):
+    response = test_client.get(f'/messages/last/{count_messages}')
+    assert response.status_code == 404
+    if count_messages == 'abc':
+        assert 'Count of last messages must be a number' in response.text
+    else:
+        assert 'Count of last messages must be less that 50 and more 0' in response.text
 
 
 def test_last_messages(test_client: TestClient):
     with test_client.websocket_connect('/ws/name') as ws:
         ws.send_text('message')
-    response = test_client.get('/messages/last50')
+    response = test_client.get('/messages/last/50')
     assert response.status_code == 200
     assert 'name' in response.text and 'message' in response.text
 
@@ -32,3 +37,12 @@ def test_ws_endpoint(test_client: TestClient):
         ws.send_text('message')
         message = ws.receive_text()
         assert message == 'name: message'
+
+
+def test_chat_logic(test_client: TestClient):
+    with test_client.websocket_connect(
+        '/ws/name1'
+    ) as ws1, test_client.websocket_connect('/ws/name2') as ws2:
+        ws1.send_text('name1 send message to name2')
+        message = ws2.receive_text()
+        assert message == 'name1: name1 send message to name2'
